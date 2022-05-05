0f5bc823a69708d56b532e4e5512ccfb7e04a77d
Narnillian <ner.baby@gmail.com>
2022-05-05 13:44:20 -0700

    This has a lot of stuff but nothing big. Mostly brainstorm

--- turing.c
+++ turing.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
-#include <sys/ioctl.h>
-#include <termios.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h> //IO control
+#include <termios.h>   //terminal interface
+char *machine;
 
-int main() {
+struct instr {
+    //char *rule = malloc(sizeof(char)*4);
+};
 
-    //get window size
-    struct winsize ws;
-    ioctl(0, TIOCGWINSZ, &ws);
-    printf("%i\n",ws.ws_col);
+struct instrset {
+
+};
+
+//"struct instr name" --> "instr name"
+typedef struct instr instr;
+
+
+
+int main(int argc, char **argv) {
+    //(f04Lf,14Lf,23Rf,32Lf,40Rf,50Rb)(*b05LB,10Rb,25LB,34,Rb,4H,52Rb)(F05Rf,11RF,24Rb,32RF,4H,51Rf)(B01LB,10RB,23RB,43LB,52RB)|00000005155*0000000
+    instr first;
+    machine = malloc(sizeof(char)*100);
     return 0;
+    
 }
