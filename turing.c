a3197af98c05fd1aeed61ad0811d04d792c12274
Narnillian <ner.baby@gmail.com>
2022-05-24 00:03:16 -0700

    I guess I changed some things, but mostly got statename reading working again

--- turing.c
+++ turing.c
@@ -23,17 +23,19 @@ Machine organization:
         -Aterisk indicates initial head position
 
 */
-struct instr { /* one instruction */
-    char newval; /* write value of head */
-    char movehead; /* based on moving right -- positive is right, negative is left, 0 is neither. will only move one position no matter the value (unless neither) */
-    char *newstate;
-};
+
 struct {
     char *title; /* direct title of state -- can be as long as you want, above length 0 */
     char *nickname; /*used for longer, more descriptive names for the state -- haven't done yet */
-    instr *instructions;
+    
+    struct { /* one instruction */
+        char newval; /* write value of head */
+        char movehead; /* based on moving right -- positive is right, negative is left, 0 is neither. will only move one position no matter the value (unless neither) */
+        char *newstate;
+    } *instructions;
 
 } *allstates = NULL; /* global list of all states */
+
 struct square { /* one square of the tape */
     char value;
     square *prev;
@@ -51,7 +53,7 @@ char *preset2 = NULL; /*        these will be made eventually                   
 char *preset3 = NULL; /*        there will be preset machines                   */
 char *fullspec = NULL; /*       machine we are using                            */
 int actstate; /*                active state                                    */
-int values; /*                  how many values a single tape square can have   */
+//int values; /*                  how many values a single tape square can have   */
 
 
 int main(int argc, char **argv) {
@@ -66,7 +68,7 @@ int main(int argc, char **argv) {
 int loadinstrs() {
     char *split = NULL;
     int states = 0;
-    int instr = 0;
+    int instrs = 0;
     char instrpart;
     for (char *readchar = fullspec; *readchar; readchar++) {
         switch (*readchar) {
@@ -82,48 +84,55 @@ int loadinstrs() {
                 }
                 allstates = realloc(allstates, (states+1)*sizeof(*allstates));
                 allstates[states].instructions = NULL;
+                allstates[states].instructions = realloc(allstates[states].instructions, (instrs+1)*sizeof(*allstates[states].instructions));
                 if (*(readchar+1)=='*') {
                     actstate = states;
                     readchar++;
                 }
-                {
+                { /* iter only exists inside these braces */
+                    allstates[states].title = NULL;
                     int iter = 0;
                     while(++iter) {
                         readchar++;
                         if (*readchar == ';') break;
+                        printf("(%d\n>%d\n<%d\n!!%d\n",states,iter,sizeof(*allstates[states].title),iter*sizeof(*allstates[states].title));
                         allstates[states].title = realloc(allstates[states].title, iter*sizeof(*allstates[states].title));
                         allstates[states].title[iter-1] = *readchar;
                     }
+                    allstates[states].title[iter-1] = 0;
                 }
-                instr=0;
-                values++;
+                //values++;
+                instrs = 0;
                 break;
             case ')':
-                instr++;
                 states++;
-                if (verbose) printf("There are \033[42m%d instructions per state\033[49m\n", instr+1);
+                if (verbose) printf("There are \033[42m%d instructions per state\033[49m\n", ++instrs);
                 break;
             case ',':
                 /* end of instruction */
                 if (verbose) {
-                    printf("^%d\n^%d\n", sizeof(*allstates[states].instructions), (instr+1)*sizeof(*allstates[states].instructions));
-                    printf("There are \033[42m%d instructions per state\033[49m\n", instr+1);
+                    printf("^%d\n^%d\n", sizeof(*allstates[states].instructions), (instrs+1)*sizeof(*allstates[states].instructions));
+                    printf("There are \033[42m%d instructions per state\033[49m\n", instrs+1);
                 }
-                allstates[states].instructions = realloc(allstates[states].instructions, (instr+1)*sizeof(*allstates[states].instructions));
+                allstates[states].instructions = realloc(allstates[states].instructions, (instrs+1)*sizeof(*allstates[states].instructions));
                 if (verbose) printf("\033[41m%p\033[49m\n",allstates[states]);
-                instr++;
+                instrs++;
                 readchar++; /* because the first character of the instruction is the indicator -- i.e. when it's called */
                 break;
-            //default:
-                
-                    /* code */
+            default:
+                //printf("%d || %d\n",states,instrs);
+                //printf("%p\n",allstates[states].instructions);
+                allstates[states].instructions[instrs].newval = *readchar;
         }
     }
     if (verbose) printf("\n\n");
     printf("There are %d states\n",states);
-    printf("There are %d insructions per state\n", instr);
+    printf("There are %d insructions per state\n", instrs);
+    printf("Name of Set [0]: '%s'\n", allstates[0].title); /* f -- THIS IS WORKING */
     printf("Name of Set [1]: '%s'\n", allstates[1].title); /* b -- THIS IS WORKING */
-    //printf("Instruction [2] at Set [1]: %c\n", statelist[1].instructions[2]); /* 25LB */
+    printf("Name of Set [2]: '%s'\n", allstates[2].title); /* F -- THIS IS WORKING */
+    printf("Name of Set [3]: '%s'\n", allstates[3].title); /* B -- THIS IS WORKING */
+    //printf("Instruction [2] at Set [1]: %c\n", allstates[1].instructions[2]); /* 25LB */
     return 0;
 }
 
