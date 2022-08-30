3fdd4c4f41ceee2322df8a488140b20fdd432669
Narnillian <ner.baby@gmail.com>
2022-08-29 18:56:21 -0700

    FINISHED INSTRUCTION LOADING WOOOOOOOOOOOHOOOOOOOOOOOOOOOOOO Finally, jeez

--- turing.c
+++ turing.c
@@ -28,10 +28,10 @@ struct {
     char *nickname; /*used for longer, more descriptive names for the state -- haven't done yet */
     
     struct { /* one instruction */
-        unsigned int halt:1; /* positive value if instruction is HALT */
+        unsigned int halt:1; /* HALT = 1, !HALT = 0 */
         unsigned int movehead:2; /* 01 means RIGHT, 11 means LEFT, 00 means NONE */
-        char newval; /* write value of head */
-        char newstate[3];
+        char newval; /* value to write at HEAD */
+        char newstate[3]; /* title of new interperative state to change to */
     } *instructions;
 
 } *allstates = NULL; /* global list of all states */
@@ -53,7 +53,7 @@ char *preset2 = NULL; /*          these will be made eventually                 
 char *preset3 = NULL; /*          there will be preset machines                 */  /* memory, and other people could use the special features i have planned */
 char *fullspec = NULL; /*       machine we are using                            */
 int actstate = 0; /*            active state                                    */
-//int values; /*                  how many values a single tape square can have   */
+int values; /*                  how many values a single tape square can have   */
 
 
 int main(int argc, char **argv) {
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
 }
 
 void *n_realloc(void *ptr, size_t size) {
-    ptr = NULL;
+    //ptr = NULL;
     void *tmp = realloc(ptr,size);
     if (!tmp) {
         printf("YOU HAD A PROBLEM REALLOCING");
@@ -95,15 +95,21 @@ int loadinstrs() {
 
 
         /* new set */
+        if (*readchar == '(') {
         printf("OPEN-PAREN\n");
         if (verbose) {
             printf(">%d\n>%d\n", sizeof(*allstates), (states+1)*sizeof(*allstates));
             printf("There are \033[46m%d instruction sets\033[0m\n", states+1);
         }
         allstates = n_realloc(allstates, (states+1)*sizeof(*allstates));
-        //allstates[states].instructions = NULL;
+        allstates[states].instructions = NULL;
         allstates[states].instructions = n_realloc(allstates[states].instructions, (instrs+1)*sizeof(*allstates[states].instructions));
+        } else {
+            printf("MACHINE NOT CONFIGURED PROPERLY\n");
+            exit(99);
+        }
         if (*(readchar+1)=='*') {
+            printf("                                   hi?\n");
             actstate = states;
             readchar++;
         }
@@ -191,26 +197,28 @@ int loadinstrs() {
                 readchar++;
                 continue;
             }
-            readchar++;
+            //readchar++;
             break;
         }
         printf("CLOSE-PAREN\n");
         states++;
-        if (verbose) printf("There are \033[42m%d instructions per state\033[0m\n", ++instrs);
-        readchar++;
-        if (*(readchar-1) == '|') break;
+        instrs++;
+        if (verbose) printf("There are \033[42m%d instructions per state\033[0m\n", instrs);
+        if (*(readchar++) == '|') break;
 
     }
-    if (verbose) printf("\n\n");
+    if (verbose) printf("\n");
+    printf("\n");
     printf("There are %d states\n",states);
     printf("There are %d insructions per state\n", instrs);
-    printf("Name of Set [0]: '%s'\n", allstates[0].title); /* f -- THIS IS WORKING */
-    printf("Name of Set [1]: '%s'\n", allstates[1].title); /* b -- THIS IS WORKING */
-    printf("Name of Set [2]: '%s'\n", allstates[2].title); /* F -- THIS IS WORKING */
-    printf("Name of Set [3]: '%s'\n", allstates[3].title); /* B -- THIS IS WORKING */
+    printf("SIZE: %d\n",sizeof(allstates[0].title));
+    printf("ACTIVE: %d\n",states);
+    printf("Name of Set [0]: '%s'\n", allstates[0].title); /* f */
+    printf("Name of Set [1]: '%s'\n", allstates[1].title); /* b */
+    printf("Name of Set [2]: '%s'\n", allstates[2].title); /* F */
+    printf("Name of Set [3]: '%s'\n", allstates[3].title); /* B */
     printf("New value of instruction [0] in set [0]: %c\n", allstates[0].instructions[0].newval);
     printf("New state of instruction [5] in set [2]: %c\n", allstates[2].instructions[5].newstate);
-    //printf("Instruction [2] at Set [1]: %c\n", allstates[1].instructions[2]); /* 5LB */
     return 0;
 }
 
