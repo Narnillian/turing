ea10c75394be0b8cc9d8303238e6c3404175f89b
Narnillian <ner.baby@gmail.com>
2022-10-19 23:01:07 -0700

    My attempt (doesn't compile)

--- turing.c
+++ turing.c
@@ -4,6 +4,8 @@
 //#include <termios.h>   /* terminal interface */
 typedef struct square square;
 size_t strlen(const char *s);
+int strcmp(const char *s1, const char *s2);
+
 
 /*
 Machine organization:
@@ -52,14 +54,14 @@ int runmachine();
 int freevars();
 
 u_int8_t verbose = 0;
-u_int8_t xverbose = 0;
 char *preset1 = "(f;4Lf,4Lf,3Rf,2Lf,0Rf,0Rb)(*b;5LB,0Rb,5LB,4Rb,H,2Rb)(F;5Rf,1RF,4Rb,2RF,H,1Rf)(B;1LB,0RB,3LF,4RB,3LB,2RB)|00000005155*0000000";
 char *preset2 = NULL; /*          these will be made eventually (base10 adder?) */  /* i may want to make them external files, so it doesn't take up too much */
 char *preset3 = NULL; /*          there will be preset machines (base2  adder?) */  /* memory, and other people could use the special features i have planned */
 char *fullspec = NULL; /*       machine we are using                            */
 int actstate = 0; /*            active state                                    */
 square *head = NULL; /*         which square the head is pointing to on the tape*/
-int values; /*                  how many values a single tape square can have   */
+char **statenames = NULL;
+int    *stateindex = NULL;
 
 
 int main(int argc, char **argv) {
@@ -69,7 +71,7 @@ int main(int argc, char **argv) {
     //printf("Instruction loading:\n\n");
     loadinstrs();
     loadtape();
-    //runmachine();
+    runmachine();
     freevars();
     return 0;
 }
@@ -90,26 +92,85 @@ int loadinstrs() {
     char *readchar = fullspec;
 
     while (*readchar != '|') {
-        /* new set */
+        /* new state */
         if (*readchar == '(') {
             allstates = n_realloc(allstates, (states+1)*sizeof(*allstates));
             allstates[states].instructions = NULL;
             allstates[states].instructions = n_realloc(allstates[states].instructions, (instrs+1)*sizeof(*allstates[states].instructions));
+            
+            statenames = n_realloc(statenames, states+1);                /* */ 
+                statenames[states] = NULL;                              /* */
+                statenames[states] = n_realloc(statenames[states], 3); /* */
+                statenames[states] = "\0\0\0";                        /* */
+            stateindex = n_realloc(stateindex, states+1);
+                stateindex[states] = 0;
         } else {
             printf("MACHINE NOT CONFIGURED PROPERLY\n");
             exit(99);
         }
+        /* active state */
         if (*(readchar+1)=='*') {
             actstate = states;
             readchar++;
         }
-        {
+        { /* state title */
             int iter = 0;
             while (*++readchar != ';') {
                 if (iter < 3) {
                     allstates[states].title[iter++] = *readchar;
                 }
             }
+
+
+            /* find place to insert new state */
+            int insert = 0;
+            int midpoint = 0; /* as is: runs through the loop once to get to the real midpoint. change it to `int midpoint = states/2` to reduce that one loop */
+            int prevmid = states;
+            if (states != 0) while (1) {
+                if (strcmp(statenames[midpoint],allstates[states].title) > 0 && strcmp(statenames[midpoint-1],allstates[states].title) < 0) {
+                    printf("found\n");
+                    insert = midpoint;
+                    break;
+                }
+                if (strcmp(statenames[midpoint],allstates[states].title) < 0) /* statenames[midpoint] is alphabetically before allstates[states].title */ {
+                    printf("before\n");
+                    midpoint/=2;
+                    continue;
+                }
+                if (strcmp(statenames[midpoint],allstates[states].title) > 0) /* statenames[midpoint] is alphabetically after  allstates[states].title */ {
+                    printf("after\n");
+                    int tmp = midpoint;
+                    midpoint = prevmid+((states-prevmid)/2);
+                    prevmid = tmp;
+                    continue;
+                }
+            }
+            /* insert new state */
+            
+
+            //char hold[3];
+            //int i = 0;
+            //do {
+            //    char tmp[3];
+            //    for (int j=0;j<3;j++) { /* tmp = *statenames[midpoint+i]; */
+            //        tmp[j]=statenames[midpoint+i+j];
+            //    }
+            //    statenames[midpoint+i] = hold;
+            //    *hold = *tmp;
+            //    i++;
+            //} while (statenames[i]);
+
+            //printf("%d ~ %d\n",states,sizeof(statenames));
+            //char tmp[3];
+            ///* i have a basic idea. flesh it out on paper */
+            //for (int i = 0; *statenames[i]; i++) {
+            //    if (strcmp(statenames[i],allstates[states].title) < 0) { /* statenames[i] is alphabetically before allstates[states].title */
+            //        *tmp = *statenames[i];
+            //        //statenames[i]
+
+            //    }
+            //}
+
         }
         readchar++;
         instrs = 0;
@@ -164,7 +225,7 @@ int loadinstrs() {
         }
         states++;
         instrs++;
-        if (*(readchar++) == '|') break;
+        readchar++;
 
     }
     fullspec = readchar;
@@ -210,7 +271,7 @@ int loadtape() {
         squares++;
         
     }
-    char *tapestring;
+           char *tapestring;
     printf("%s\n", tapestring = tapetostring()); free(tapestring);/* not permanent */
 
     return 0;
@@ -241,16 +302,19 @@ char *tapetostring() {
 
 
 int runmachine() {
-    while (1) {
-        head->value = allstates[actstate].instructions[atoi(&head->value)].newval;
-        /* fix actstate's design, then put other stuff here */
+    //while (allstates[actstate].instructions->halt != 1) {
+    //    head->value = allstates[actstate].instructions[atoi(&head->value)].newval;
+    //    /* fix actstate's design, then put other stuff here */
 
-    }
+    //}
+    return 0;
 }
 
 int freevars() {
     /* it might be good practice to free all the parts of allstates */
     free(allstates);
     free(tape);
+    free(statenames);
+    free(stateindex);
     return 0;
 }
